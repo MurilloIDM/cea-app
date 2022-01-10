53e9b56148f5d76fc8763cd39ba91d9e830b0644
Gustavo780904 <gustavodutrasantos@gmail.com>
2022-01-09 23:52:44 -0300

    criado o componente TitlePage

--- src/components/TitlePage/index.js
+++ src/components/TitlePage/index.js
@@ -1,13 +1,6 @@
 import { Text, View } from 'react-native';
 import styles from './styles';
 
-// const Texto = (props) => {
-//     return (
-//       <View style={{alignItems: 'center'}}>
-//         <Text> {props.text}</Text>
-//       </View>
-//     );
-// }
 const TitlePage = ({text}) => {
     return (
         <View>
@@ -17,15 +10,3 @@ const TitlePage = ({text}) => {
 }
 
 export default TitlePage;
-
-
-
-// export default LotsOfGreetings = () => {
-//     return (
-//       <View style={{alignItems: 'center', top: 50}}>
-//         <Greeting name='Rexxar' />
-//         <Greeting name='Jaina' />
-//         <Greeting name='Valeera' />
-//       </View>
-//     );
-// }
