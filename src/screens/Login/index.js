0ad05f0acf1e4d5015d84414dafb0ee0ca5826b9
Cristina Kochmann <cristinak.iftm@gmail.com>
2022-04-27 20:04:42 -0300

    New: created screen Login-email

---
+++ src/screens/Login/index.js
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import { Text, View, Image, KeyboardAvoidingView, ScrollView} from 'react-native';
+
+import Input from "../../components/Input";
+import Button from "../../components/Button";
+import TagError from "../../components/TagError";
+
+import { validateLogin } from "../../utils/login";
+
+import logo from "../../../assets/images/logo.png";
+
+import styles from "./styles";
+
+const Login = ({}) => {
+
+    const [email, setEmail] = useState("");
+    const [errorField, setErrorField] = useState(false);
+
+    const handleChangeEmail = (value) => setEmail(value);
+
+    const clearFields = () => {
+        setEmail("");
+        setErrorField(false);
+    };
+    
+    const submitForm = async () => {
+        setErrorField(false);
+
+        const values = { email };
+        const { success, message } = validateLogin(values);
+        
+        if (!success) {
+
+            clearFields();
+            setErrorField(message);
+            return;
+        }
+
+        const payload = {
+            email,
+        };
+        
+        console.log(payload);
+    }
+        
+    return(
+        <KeyboardAvoidingView behavior="height">
+            
+            <View style={styles.container}>
+                <View>
+                    <Image
+                    resizeMode="contain"
+                    style={styles.logo}
+                    source={logo}
+                    />
+                </View>
+
+                <View>
+                    <Text style={styles.title}>
+                        Seja bem vindo à nossa comunidade!
+                    </Text>
+
+                    <Text style={styles.description}>
+                        Digite seu e-mail para começar.
+                    </Text>
+                </View>
+
+                <View style={styles.form}>
+                    <Input
+                        value={email}
+                        required={false}
+                        onChange={handleChangeEmail}
+                        type="email-address"
+                        placeholder="pessoa@gmail.com"
+                    
+                    />
+                    
+                    {errorField && (
+                        <TagError description={errorField} />
+                    )}
+
+                    <Button
+                        text="Próximo"
+                        stylesText={styles.buttonSubscribe}
+                        handleOnPress={submitForm}
+                    />
+                </View>
+            </View>
+            
+        </KeyboardAvoidingView>
+    );
+
+};
+
+export default Login;
---
+++ src/utils/login.js
@@ -0,0 +1,22 @@
+const REGEX_EMAIL = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/gi;
+
+export const validateLogin = ({email}) =>{
+
+    if (!email) {
+        return {
+          success: false,
+          message: "O E-mail não pode estar em branco! Favor preenchê-lo.",
+        };
+      }
+
+    const emailRegex = new RegExp(REGEX_EMAIL);
+
+    if (!emailRegex.test(email)) {
+    return {
+        success: false,
+        message: "O valor informado para o E-mail é inválido! Digite um e-mail em um formato válido.",
+    };
+    }
+    
+    return { success: true, message: "" };
+}
